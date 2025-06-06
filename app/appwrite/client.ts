39a86ff7c7353f195397af1677dacef459b2efbf
UdyanDSuradkar <suradkarudyan18@gmail.com>
2025-06-06 12:45:13 +0530

    Implementing dashboard Layout & UI, Auth functionality

--- app/appwrite/client.ts
+++ app/appwrite/client.ts
@@ -1,3 +1,5 @@
+import { Client, Account, Databases, Storage } from "appwrite";
+
 export const appwriteConfig = {
   endpointUrl: import.meta.env.VITE_APPWRITE_ENDPOINT,
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
@@ -6,3 +8,13 @@ export const appwriteConfig = {
   userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
   tripCollectionId: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION_ID,
 };
+
+const client = new Client()
+  .setEndpoint(appwriteConfig.endpointUrl)
+  .setProject(appwriteConfig.projectId);
+
+const account = new Account(client);
+const database = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, database, storage };
