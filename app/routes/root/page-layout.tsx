677fbac4312d64b947333077a5d87283a3c6a1b5
UdyanDSuradkar <suradkarudyan18@gmail.com>
2025-06-14 14:47:12 +0530

    Admin Dashboard completed

---
+++ app/routes/root/page-layout.tsx
@@ -0,0 +1,26 @@
+import { Outlet, redirect, useNavigate } from "react-router";
+import { getExistingUser, logoutUser, storeUserData } from "~/appwrite/auth";
+import { account } from "~/appwrite/client";
+
+export async function clientLoader() {
+  try {
+    const user = await account.get();
+
+    if (!user.$id) return redirect("/sign-in");
+
+    const existingUser = await getExistingUser(user.$id);
+    return existingUser?.$id ? existingUser : await storeUserData();
+  } catch (e) {
+    console.log("Error fetching user", e);
+    return redirect("/sign-in");
+  }
+}
+
+const PageLayout = () => {
+  return (
+    <div className="bg-light-200">
+      <Outlet />
+    </div>
+  );
+};
+export default PageLayout;
